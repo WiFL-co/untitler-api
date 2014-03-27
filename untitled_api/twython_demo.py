39762b278393cf35a8faf8d54f0f6f49b99054cc
Scott Coates <scoarescoare@gmail.com>
2014-03-26 23:40:24 -0400

    updated serach

--- untitled_api/twython_demo.py
+++ untitled_api/twython_demo.py
@@ -1,9 +1,12 @@
 import asyncio
 import datetime
+from itertools import groupby
 import json
+from operator import attrgetter
 import textwrap
 import threading
 import os
+from dateutil.relativedelta import relativedelta
 
 from twython import Twython
 
@@ -18,6 +21,7 @@ current_day = datetime.datetime.utcnow().strftime("%Y-%m-%d")
 keywords = textwrap.dedent("""\
   #firebase
   #atom.io
+  #python
 """)
 search_query = " OR ".join(k for k in keywords.splitlines())
 
@@ -26,16 +30,71 @@ def get_tweets():
   return twitter.search(q=search_query, result_type='mixed', since=current_day, lang="en", include_entities=False)
 
 
+def acceptable_tweet(tweet):
+  ret_val = True
+
+  if ret_val:
+    if tweet['retweet_count'] >= 20 or tweet['favorite_count'] >= 20:
+      ret_val = False
+
+  if ret_val:
+    if tweet['user']['statuses_count'] < 50:
+      ret_val = False
+
+  if ret_val:
+    followers_count = tweet['user']['followers_count']
+    following_count = tweet['user']['friends_count']
+
+    if following_count >= 10:
+      if followers_count > 500:
+        ret_val = (following_count / followers_count) >= .65
+    else:
+      ret_val = False
+
+  return ret_val
+
+
+@asyncio.coroutine
+def acceptable_tweets(tweets):
+  ret_val = [tweet for tweet in tweets if acceptable_tweet(tweet)]
+  usernames = [tweet['user']['screen_name'] for tweet in ret_val]
+  acceptable_users = " OR ".join(["from:" + un for un in usernames])
+  past_week = (datetime.datetime.utcnow() - relativedelta(weeks=1)).strftime("%Y-%m-%d")
+
+  recent_user_tweets = yield from loop.run_in_executor(
+    None,
+    lambda: twitter.search(q=acceptable_users, result_type='recent',
+                           since=past_week, lang="en",
+                           include_entities=False)
+  )
+
+  name_key = lambda tweet: tweet['user']['screen_name']
+
+  recent_user_tweets = sorted(recent_user_tweets['statuses'], key=name_key)
+
+  user_grouped_tweets = {key: len(list(value)) for key, value in (groupby(recent_user_tweets, name_key))}
+  user_grouped_tweets = {key: value for key, value in user_grouped_tweets.items() if value >= 2}
+
+  ret_val = [tweet for tweet in ret_val if tweet['user']['screen_name'] in user_grouped_tweets]
+  return ret_val
+
+
 @asyncio.coroutine
 def main():
   print('starting')
 
-  python_tweets = yield from  loop.run_in_executor(None, get_tweets)
+  python_tweets = yield from loop.run_in_executor(None, get_tweets)
 
-  print(json.dumps(python_tweets['statuses']))
+  python_tweets = python_tweets['statuses']
 
-  # for tweet in python_tweets['statuses']:
-  # pprint(tweet)
+  python_tweets = yield from acceptable_tweets(python_tweets)
+
+  for tweet in python_tweets:
+    print("User: {0} ---- {1}".format(tweet['user']['screen_name'], tweet['text']))
+
+
+    # for tweet in python_tweets['statuses']:
+    # pprint(tweet)
 
 
 @asyncio.coroutine
